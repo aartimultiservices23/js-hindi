3530cdc18003b1043596989be0a5a3336acae9e8
aartimultiservices23 <aartimultiservices23@gmail.com>
2025-01-23 12:05:23 +0530

    datatypes and conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3+3)   we are using node js not web browser
+
+
+let age = 18
+let isLoggedIn = false
+let state = ""
+
+
+// premitive datatypes --->
+// number --> 2 to power 53
+// bigint--> big integer
+// string -->""
+// boolean --> true/false
+
+// null --> standalone value
+// undefined -->
+// symbol -->unique
+
+
+// object 
+
+console.log(typeof "shubham");
+console.log(typeof null); //it gives an object
+console.log(typeof undefined); //it gives an undefined
+
+
+console.log(typeof 3.14);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,108 @@
+
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+
+
+// let score = true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+
+
+// let score = false
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+
+
+// "33" --> 33
+// "33abc" --> NaN
+// true --> 1; False -->0
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+// let isLoggedIn = 0;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = "shubham";   treu
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+
+let isLoggedIn = "";    //false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 -->true; 0 --> false
+// "" --> false
+// "hitesh"  -->true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
